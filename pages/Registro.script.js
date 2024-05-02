efccb36ca7ea6888f1d23adb9e7ad99c1f080992
axelvilla <axelvilla746@gmail.com>
2024-05-02 14:23:17 -0300

    Solucionado el error de login con navbar lateral

--- pages/Registro.script.js
+++ pages/Registro.script.js
@@ -1,3 +1,29 @@
+let signupBtn = document.getElementById('signupBtn');
+let signinBtn = document.getElementById('signinBtn');
+let nameField = document.getElementById('nameField');
+let passwordField = document.getElementById('passwordField').parentElement;
+let forgotPasswordBtn = document.getElementById('forgot-password');
+let title = document.getElementById('title');
+
+signinBtn.onclick = function(){
+    nameField.style.maxHeight = '0';
+    passwordField.style.maxHeight = '0';
+    title.innerHTML = 'Sign In';
+    signupBtn.classList.add('disable');
+    signinBtn.classList.remove('disable');
+    forgotPasswordBtn.innerHTML = 'Forgot Password?';
+}
+
+signupBtn.onclick = function(){
+    nameField.style.maxHeight = '65px';
+    passwordField.style.maxHeight = '65px';
+    title.innerHTML = 'Sign up';
+    signupBtn.classList.remove('disable');
+    signinBtn.classList.add('disable');
+    forgotPasswordBtn.innerHTML = '';
+}
+
+
 
 // --------------------------------------------------------
 //MENU COPIADO DESDE EL HEADER
@@ -42,30 +68,6 @@ window.addEventListener("resize", function(){
 
 
 
-let signupBtn = document.getElementById('signupBtn');
-let signinBtn = document.getElementById('signinBtn');
-let nameField = document.getElementById('nameField');
-let passwordField = document.getElementById('passwordField').parentElement;
-let forgotPasswordBtn = document.getElementById('forgot-password');
-let title = document.getElementById('title');
-
-signinBtn.onclick = function(){
-    nameField.style.maxHeight = '0';
-    passwordField.style.maxHeight = '0';
-    title.innerHTML = 'Sign In';
-    signupBtn.classList.add('disable');
-    signinBtn.classList.remove('disable');
-    forgotPasswordBtn.innerHTML = 'Forgot Password?';
-}
-
-signupBtn.onclick = function(){
-    nameField.style.maxHeight = '65px';
-    passwordField.style.maxHeight = '65px';
-    title.innerHTML = 'Sign up';
-    signupBtn.classList.remove('disable');
-    signinBtn.classList.add('disable');
-    forgotPasswordBtn.innerHTML = '';
-}
 
 
 
